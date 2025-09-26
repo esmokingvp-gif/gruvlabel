738ceae555c5212444e4e71d4b38f1d278eb585d
esmokingvp-gif <esmokingvp@gmail.com>
2025-09-26 19:19:44 +0000

    dfloaopkdas2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// REMOVIDO: A importação do tailwindcss foi deletada daqui
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
@@ -10,6 +12,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+      // REMOVIDO: O bloco 'css' foi removido. O Vite vai ler o postcss.config.js automaticamente.
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
